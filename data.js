93de94f6ff0fc6fc4daa9b5338383849e5bf41e9
Jablaw <jbenjamin@thejablaw.com>
2025-03-06 19:37:50 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,45 @@
+const dateOptions = {
+  "Movies": [
+    { name: "Aladdin", link: "https://www.levidia.ch/movie.php?watch=aladdin" },
+    { name: "Aladdin Live Action", link: "https://www.levidia.ch/movie.php?watch=aladdin-6139732" },
+    { name: "Alice in Wonderland", link: "https://www.levidia.ch/movie.php?watch=alice-in-wonderland" },
+    { name: "The Notebook", link: "https://www.levidia.ch/movie.php?watch=the-notebook" },
+    { name: "Parasite", link: "https://www.levidia.ch/movie.php?watch=parasite-6751668" },
+    { name: "Straight Outta Compton", link: "https://www.levidia.ch/movie.php?watch=straight-outta-compton" },
+    { name: "My Neighbor Totoro", link: "https://www.levidia.ch/movie.php?watch=tonari-no-totoro-1988-" },
+    { name: "The Absolute Saddest Movie of All Time", link: "https://www.levidia.ch/movie.php?watch=hotaru-no-haka-1988-" },
+    { name: "Spider-Man: across the Spider-Verse", link: "https://www.levidia.ch/movie.php?watch=spiderman-across-the-spiderverse" },
+    { name: "anora", link: "https://www.levidia.ch/movie.php?watch=anora" },
+    { name: "The Social Network", link: "https://www.netflix.com/" }
+  ],
+  "TV Shows": [
+    { name: "Breaking Bad", link: "https://www.netflix.com" },
+    { name: "Invincible", link: "https://www.levidia.ch/tv-show.php?watch=invincible" },
+    { name: "Abbott Elementary", link: "https://www.levidia.ch/tv-show.php?watch=abbott-elementary" },
+    { name: "Brooklyn Nine-Nine", link: "https://www.levidia.ch/tv-show.php?watch=brooklyn-ninenine" },
+    { name: "Friends", link: "https://www.levidia.ch/tv-show.php?watch=friends" },
+    { name: "The Office (U.S.)", link: "https://www.levidia.ch/tv-show.php?watch=the-office" },
+    { name: "Stranger Things", link: "https://www.netflix.com/" },
+    { name: "Breaking Bad", link: "https://www.netflix.com/" },
+    { name: "The Mandalorian", link: "https://www.levidia.ch/tv-show.php?watch=the-mandalorian" },
+    { name: "Sherlock", link: "https://www.amazon.com/" },
+    { name: "How I Met Your Mother", link: "https://www.levidia.ch/tv-show.php?watch=how-i-met-your-mother" },
+    { name: "Avatar: The Last Airbender", link: "http://levidia.ch/tv-show.php?watch=avatar-the-last-airbender" },
+    { name: "Love is Blind", link: "https://www.netflix.com/" }
+  ],
+  "Games": [
+    { name: "Minecraft", link: "https://www.minecraft.net" },
+    { name: "Among Us", link: "https://www.steam.com" },
+    { name: "Overcooked 2", link: "https://store.steampowered.com" },
+    { name: "Skribbl.io", link: "https://skribbl.io/" },
+    { name: "Gartic Phone", link: "https://garticphone.com/" },
+    { name: "Codenames", link: "https://codenames.game/" },
+    { name: "Agar.io", link: "https://agar.io/" },
+    { name: "Krunker.io", link: "https://krunker.io/" },
+    { name: "Tetris (Tetr.io)", link: "https://tetr.io/" },
+    { name: "UNO Online", link: "https://play.unofreak.com/" },
+    { name: "Chess (Chess.com)", link: "https://www.chess.com/" },
+    { name: "Geoguessr (Free Mode)", link: "https://www.geoguessr.com/free" },
+    { name: "Board Game Arena", link: "https://boardgamearena.com/" }
+  ]
+};
